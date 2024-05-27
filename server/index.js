77e14fae5724191c986790d4a23222de7ff2adeb
Karin1412 <100667390+Karin1412@users.noreply.github.com>
2024-05-27 23:54:02 +0700

    update server

---
+++ server/index.js
@@ -0,0 +1,27 @@
+// index.js
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.get('/', (req, res) => {
+  res.send('Chat Server is running');
+});
+
+io.on('connection', (socket) => {
+  console.log('New client connected');
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 4000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
